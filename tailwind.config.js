742e91988f02b614b4de7e8ada7678df18c3a7ee
rknoche6 <rknoche@purdue.edu>
2021-12-06 14:06:36 +0100

    Started working on header

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+
+    './src/**/*.html',
+
+    './src/**/*.js',
+
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
